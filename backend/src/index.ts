0b4e3c7d2211bd2ca0c5a61810f95c283b861263
Sahil Kumar Ray <Piyushkrray1@gmail.com>
2024-07-14 01:07:00 +0530

    chore: update backend + commons

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,23 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign, verify } from 'hono/jwt';
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+    JWT_SECRET: string
+	}
+}>();
+
+app.route("api/v1/user", userRouter);
+app.route("api/v1/blog", blogRouter);
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
-app.post('/api/v1/signup', (c)=>{
-  return c.text('Hello Hono!')
-})
-
-app.post('/api/v1/signin', (c)=>{
-  return c.text('Hello Hono!')
-
-})
-
-app.post('/api/v1/blog', (c)=>{
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog/;id', (c)=>{
-  return c.text('Hello Hono!')
-})
-
 
 export default app
